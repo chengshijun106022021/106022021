0dbbe816db4941f9efc9d9cd8f4b5bf7af40209f
chengshijun106022021 <j.j.jack.yo@gmail.com>
2017-09-14 14:43:32 +0800

    name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("My First Java Code!!2");
+	System.out.print("學號:106022021\n系級:行動商務與多媒體應用學系一年級\n姓名:程世駿\n時間;2017/09/14\n地點:I319");
     }
 }
